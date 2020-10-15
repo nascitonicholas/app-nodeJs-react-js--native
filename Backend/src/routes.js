f63d117c66bd1f379ad8dbc3bc310da4024bf82c
nascito_nicholas <nascito.nicholas@gmail.com>
2020-10-15 15:39:53 -0300

    Criação de mais rotas para a manutenção dos casos e das ongs, habilitando o cors.

--- Backend/src/routes.js
+++ Backend/src/routes.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const ongController = require('./controllers/OngController');
 const incidentController = require('./controllers/IncidentController');
+const profileController = require('./controllers/ProfileController');
+const sessionController = require('./controllers/SessionController');
 const routes = express.Router();
+
+routes.post('/session', sessionController.create);
 
 routes.get('/ongs', ongController.index);
 routes.post('/ongs', ongController.create);
+
+routes.get('/profile', profileController.index);
 
 routes.get('/incidents', incidentController.index);
 routes.post('/incidents', incidentController.create);
