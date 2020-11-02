205a239843a0160328d651de72631bbeede492f7
nascito_nicholas <nascito.nicholas@gmail.com>
2020-11-02 17:45:39 -0300

    Conectando páginas as rotas da API e deixando dinamico as páginas.

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,18 +1,34 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import { FiLogIn } from 'react-icons/fi';
 import './style.css';
 import heroesImg from '../../assets/heroes.png';
 import logoImg from '../../assets/logo.svg';
+import api from '../../services/api';
 
 export default function Logon() {
+  const [id, setId] = useState('');
+  const history = useHistory();
+
+  async function handleLogin(e) {
+    e.preventDefault();
+    try {
+      const response = await api.post('session', { id });
+      localStorage.setItem('ongId', id);
+      localStorage.setItem('ongName', response.data.name);
+      history.push('/profile');
+    } catch (e) {
+      alert('Falha no login, tente novamente.')
+    }
+  }
+
   return (
     <div className="logon-container">
       <section className="form">
         <img src={logoImg} alt="Be The Hero"/>
-        <form action="">
+        <form onSubmit={handleLogin}>
           <h1>Faça seu logon</h1>
-          <input type="text" placeholder="Sua ID"/>
+          <input type="text" placeholder="Sua ID" value={id} onChange={e => setId(e.target.value)} />
           <button className="button" type="submit">Entrar</button>
           <Link className="back-link" to="/register">
             <FiLogIn size={16} color="#E02041" />
